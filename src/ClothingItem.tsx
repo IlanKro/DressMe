102fce70264ce6380d3f7dd148bb6e66fc02124e
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2021-10-27 18:58:49 +0300

    Implemented item selection

--- src/ClothingItem.tsx
+++ src/ClothingItem.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   SafeAreaView,
   FlatList,
+  Alert,
 } from "react-native";
 import { ClothingItem, RootStackParamList } from "../App";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -22,32 +23,88 @@ export default function ClothingItemComponent({ route, navigation }: Props) {
   const [searchCategory, setSearchCategory] = useState<SearchOptions>("name");
   const [filteredData, SetFilteredData] = useState<ClothingItem[]>([]);
   const [data, setData] = useState<ClothingItem[]>([]);
+  const [color, setColor] = useState<string>("");
+  const [selected, setSelected] = useState<string>("");
 
-  const Item = ({ name, brand, colors }: any) => (
-    <View style={styles.item}>
-      <Text>name: {name}</Text>
-      <Text>brand: {brand}</Text>
-      {colors && (
-        <FlatList
-          data={addIndex(colors.sort())}
-          renderItem={renderColorButton}
-        />
-      )}
-    </View>
-  );
-
-  const renderColorButton = ({ item }: any) => {
+  const renderItem = ({ item }: any) => {
     return (
-      <Button
-        color={item.value}
-        title={item.value}
-        onPress={() => console.log("pressed")}
+      <Item
+        name={item.name}
+        brand={item.brand}
+        colors={item.colors}
+        id={item.id}
+        sizes={item.sizes}
       />
     );
   };
 
-  const renderItem = ({ item }: any) => {
-    return <Item name={item.name} brand={item.brand} colors={item.colors} />;
+  const Item = ({ name, brand, colors, id, sizes }: any) => (
+    <View style={styles.item}>
+      <Text>name: {name}</Text>
+      <Text>brand: {brand}</Text>
+      <Text>colors: </Text>
+      {id == selected && <Text>{color} selected</Text>}
+      {colors && (
+        <FlatList
+          data={addIndex(colors.sort())}
+          renderItem={(item) => renderColorButton(item, id)}
+          listKey={id + "colors"}
+        />
+      )}
+      {id == selected && (
+        <View>
+          <Text>sizes: </Text>
+          <FlatList
+            data={addIndex(sizes.sort())}
+            renderItem={(item) => renderSizesButton(item, id)}
+            listKey={id + "sizes"}
+          />
+        </View>
+      )}
+    </View>
+  );
+
+  const renderColorButton = ({ item }: any, id: string) => {
+    return (
+      <Button
+        color={item.value}
+        title={item.value}
+        onPress={() => {
+          setColor(item.value);
+          setSelected(id);
+        }}
+      />
+    );
+  };
+
+  const renderSizesButton = ({ item }: any, id: string) => {
+    return (
+      <Button
+        title={item.value.toString()}
+        onPress={() =>
+          Alert.alert(
+            "Do you want to select this item?",
+            "Once selected you will return to the home screen",
+            [
+              {
+                text: "No",
+              },
+              {
+                text: "Yes",
+                onPress: () => {
+                  let selectedItem = data.filter(
+                    (item) => String(item["id"]) == selected
+                  )[0]; //Ids are unique.
+                  selectedItem.colors = [color];
+                  selectedItem.sizes = [item.value.toString()];
+                  navigation.navigate("Home", { item: selectedItem });
+                },
+              },
+            ]
+          )
+        }
+      />
+    );
   };
 
   useEffect(() => {
@@ -60,14 +117,22 @@ export default function ClothingItemComponent({ route, navigation }: Props) {
         }
         items = items.filter((item) => item["type"] == type);
         items.sort(sortByProperty("name"));
+        //Removed duplicate ids I found some in the mock data, in actual apps the database should avoid duplicate primary keys...
+        items = [...new Set(items)];
         setData(items);
       });
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>found {data.length} items</Text>
-      {data && <FlatList data={data} renderItem={renderItem} />}
+      <Text style={styles.found}>found {data.length} items</Text>
+      {data && (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          ListEmptyComponent={<Text>no results found</Text>}
+        />
+      )}
     </SafeAreaView>
   );
 }
@@ -77,7 +142,6 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   container: {
-    alignItems: "center",
     flex: 1,
     backgroundColor: "#fff",
   },
@@ -86,6 +150,9 @@ const styles = StyleSheet.create({
     borderRadius: 2,
     borderWidth: 2,
     margin: 3,
-    flexGrow: 1,
+    flex: 2,
+  },
+  found: {
+    alignSelf: "center",
   },
 });
